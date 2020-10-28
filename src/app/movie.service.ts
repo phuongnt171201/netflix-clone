f757077d20bf35bc0bd80fb03597b68f2ad96bba
phuong171201 <phuongnt171201@gmail.com>
2020-10-28 11:58:40 +0700

    init

---
+++ src/app/movie.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+
+  constructor() { }
+
+  const API_KEY = "14df9d5f2206595b05c62b3d3d15b8e0";
+
+  getTopRate(){
+    return
+  }
+}
